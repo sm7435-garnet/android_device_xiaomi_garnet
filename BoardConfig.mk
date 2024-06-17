0261dca6200c1ce92fdf7b181014bc7fb8f2b3ff
Michael Bestas <mkbestas@lineageos.org>
2024-06-17 10:41:47 +0300

    garnet: Inherit from legacy QTI FCM
    Change-Id: I0bde1c273d4abe689f61e7d4ebcf80965d785489
    Signed-off-by: UnmoveD <unmoved2121@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,6 +69,7 @@ DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
