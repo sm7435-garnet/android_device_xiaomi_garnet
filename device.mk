ffe98f40f67f0464a6a8460ccfc77296799b5a3f
Michael Bestas <mkbestas@lineageos.org>
2024-11-25 15:19:52 -0300

    garnet: Shim libwvhidl with libcrypto_shim
    The following change
    google/boringssl@e202e51
    removed symbols that our libs require.
    
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29

--- device.mk
+++ device.mk
@@ -193,7 +193,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
-    android.hardware.drm-service.clearkey
+    android.hardware.drm-service.clearkey \
+    libcrypto_shim.vendor
 
 # Device-specific settings
 PRODUCT_PACKAGES += \
